6b98eed312e026c6b974acc85b6f20c3ab5fe958
Ryan Mellmer <ryan.mellmer@plixer.com>
2023-03-01 15:43:50 -0800

    Get basic functionality working

--- main.ts
+++ main.ts
@@ -1,85 +1,146 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownPostProcessorContext, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { around } from "monkey-around";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface LinkRangeSettings {
+	headingSeparator: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: LinkRangeSettings = {
+	headingSeparator: '..'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function checkLink(linkHTML: HTMLElement, settings: LinkRangeSettings, hrefField = "data-href"): any {
+	const linkRegex = /([^#|]*)#?([^#|]*)?/;
+
+	const href = linkHTML.getAttribute(hrefField);
+
+	if (href == null) {
+		return false;
+	}
+
+	const matches = linkRegex.exec(href)
+
+	if (matches == null || matches?.length < 3 || matches[2] == undefined) {
+		return false;
+	}
+
+	const header = matches[2];
+	const split = header.split(settings.headingSeparator);
+
+	// our ranged link format is "#h1..h2"
+	if (split.length < 2) {
+		return false;
+	}
+
+	const note = matches[1];
+	const h1 = split[0];
+	const h2 = split[1];
+
+	if (app.metadataCache != null) {
+		const foundNote : TFile | undefined = app.vault.getMarkdownFiles().filter(
+			x => x.basename == note
+		).first()
+
+		if (foundNote) {
+			const meta = app.metadataCache.getFileCache(foundNote);
+
+			const h1Line = meta?.headings?.filter(
+				h => h.heading == h1
+			).first()?.position.start.line;
+
+			const h2Line = meta?.headings?.filter(
+				h => h.heading == h2
+			).first()?.position.end;
+
+			if (!h1Line) {
+				return false;
+			}
+
+			return {
+				note,
+				h1,
+				h2,
+				h1Line,
+				h2Line
+			};
+		}
+	}
+}
+
+export default class LinkRange extends Plugin {
+	settings: LinkRangeSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new LinkRangeSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		const pagePreviewPlugin = this.app.internalPlugins.plugins["page-preview"];
+		if (!pagePreviewPlugin.enabled) return;
+
+		const settings = this.settings;
+
+		// on page load, update hrefs to strip off second header to handle clickthrough, and add new range-href field
+		this.registerMarkdownPostProcessor((el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+			const links = el.querySelectorAll('a.internal-link');
+
+			links.forEach(link => { 
+				const htmlLink = link as HTMLElement
+				const res = checkLink(htmlLink, settings);
+
+				if (res !== false) {
+					htmlLink.setAttribute("href", res.note + "#" + res.h1);
+					htmlLink.setAttribute("data-href", res.note + "#" + res.h1);
+					htmlLink.setAttribute("range-href", res.note + "#" + res.h1 + this.settings.headingSeparator + res.h2);
+				}
+			});
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		// intercept page-preview plugin
+		const uninstaller = around(pagePreviewPlugin.instance.constructor.prototype, {
+			onHoverLink(old: Function) {
+				return function (options: { event: MouseEvent }, ...args: unknown[]) {
+					return old.call(this, options, ...args);
+				};
+			},
+			onLinkHover(old: Function) {
+				return function (
+					parent: any,
+					targetEl: HTMLElement,
+					linkText: string,
+					path: string,
+					state: any,
+					...args: unknown[]
+				) {
+					// parse link using the added range-href field
+					const res = checkLink(targetEl, settings, "range-href")
+					if (res !== false) {
+						old.call(this, parent, targetEl, res.note, path, {scroll:res.h1Line}, ...args)
+					} else {
+						old.call(this, parent, targetEl, linkText, path, state, ...args);
+					}
+				};
+			},
+		});
+		this.register(uninstaller);
+	
+		// This will recycle the event handlers so that they pick up the patched onLinkHover method
+		pagePreviewPlugin.disable();
+		pagePreviewPlugin.enable();
+	
+		this.register(function () {
+			if (!pagePreviewPlugin.enabled) return;
+			pagePreviewPlugin.disable();
+			pagePreviewPlugin.enable();
+		});
 	}
 
 	onunload() {
-
+		
 	}
 
 	async loadSettings() {
@@ -91,26 +152,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class LinkRangeSettingTab extends PluginSettingTab {
+	plugin: LinkRange;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: LinkRange) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -123,14 +168,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Heading Separator')
+			.setDesc('Defines the separator to be used to define a link heading range. Defaults to ".." (i.e. [[Note Name#h1..h2]])')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.headingSeparator)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.headingSeparator = value;
 					await this.plugin.saveSettings();
 				}));
 	}
